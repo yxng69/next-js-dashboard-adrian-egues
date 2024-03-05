4e171cab042fb9920c1cca6fe9445eb5a3bead81
yxng69 <yvngxristg59@gmail.com>
2024-03-05 10:28:20 -0500

    Primer Commit
    Primer cambio

--- app/ui/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
-
-export const lusitana = Lusitana({
-     subsets:['latin'], 
-     weight:['400', '700'] });
-export const inter = Inter({ subsets: ['latin'] }) ;
